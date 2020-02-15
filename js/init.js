e75cbb539c54ea4041563a410147a88208702b17
Gosia Barlik-Bokowy <malgorzata.barlik@gmail.com>
2020-02-15 10:47:40 +0100

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({scrollOffset:200,throttle:100});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
